f37d308dfa4ba063ec31190b8ceb1fa7923c94fb
Ronald Garcia Vazquez <Rgarcia@nxtara.com>
2025-12-21 19:50:31 -0300

    ready to docker play

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,3 +1,3 @@
-export const API_AUTH_BASE_URL = 'http://localhost:8080';
-export const API_LABS_BASE_URL = 'http://localhost:8081';
-export const API_RESULTS_BASE_URL = 'http://localhost:8082';
+export const API_AUTH_BASE_URL = 'https://ip172-18-0-14-d546kba91nsg00bgpm5g-8080.direct.labs.play-with-docker.com';
+export const API_LABS_BASE_URL = 'https://ip172-18-0-14-d546kba91nsg00bgpm5g-8081.direct.labs.play-with-docker.com';
+export const API_RESULTS_BASE_URL = 'https://ip172-18-0-14-d546kba91nsg00bgpm5g-8082.direct.labs.play-with-docker.com';
