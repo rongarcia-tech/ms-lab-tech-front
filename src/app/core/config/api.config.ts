17bd0fab0eab6198fa7e4c3d0786f1d00cef0bf4
Ronald Garcia Vazquez <Rgarcia@nxtara.com>
2025-12-21 20:56:10 -0300

    change aleatory id docker play free

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,3 +1,3 @@
-export const API_AUTH_BASE_URL = 'https://ip172-18-0-14-d546kba91nsg00bgpm5g-8080.direct.labs.play-with-docker.com';
-export const API_LABS_BASE_URL = 'https://ip172-18-0-14-d546kba91nsg00bgpm5g-8081.direct.labs.play-with-docker.com';
-export const API_RESULTS_BASE_URL = 'https://ip172-18-0-14-d546kba91nsg00bgpm5g-8082.direct.labs.play-with-docker.com';
+export const API_AUTH_BASE_URL = 'https://ip172-18-0-14-d548e8q9_d548ed0l2o9000emb0hg-8080.direct.labs.play-with-docker.com';
+export const API_LABS_BASE_URL = 'https://ip172-18-0-14-d548e8q9_d548ed0l2o9000emb0hg-8081.direct.labs.play-with-docker.com';
+export const API_RESULTS_BASE_URL = 'https://ip172-18-0-14-d548e8q9_d548ed0l2o9000emb0hg-8082.direct.labs.play-with-docker.com';
