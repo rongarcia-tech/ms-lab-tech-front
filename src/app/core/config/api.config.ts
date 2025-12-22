fc786f052cd7fe90c9d71108dcf40529ab8861fd
Ronald Garcia Vazquez <Rgarcia@nxtara.com>
2025-12-21 21:04:27 -0300

    change port aleatory to docker play1

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,3 +1,3 @@
-export const API_AUTH_BASE_URL = 'https://ip172-18-0-6-d548e8q9_d548ed0l2o9000emb0hg-8080.direct.labs.play-with-docker.com';
-export const API_LABS_BASE_URL = 'https://ip172-18-0-6-d548e8q9_d548ed0l2o9000emb0hg-8081.direct.labs.play-with-docker.com';
-export const API_RESULTS_BASE_URL = 'https://ip172-18-0-6-d548e8q9_d548ed0l2o9000emb0hg-8082.direct.labs.play-with-docker.com';
+export const API_AUTH_BASE_URL = 'https://ip172-18-0-19-d548e8q9_d548ed0l2o9000emb0hg-8080.direct.labs.play-with-docker.com';
+export const API_LABS_BASE_URL = 'https://ip172-18-0-19-d548e8q9_d548ed0l2o9000emb0hg-8081.direct.labs.play-with-docker.com';
+export const API_RESULTS_BASE_URL = 'https://ip172-18-0-19-d548e8q9_d548ed0l2o9000emb0hg-8082.direct.labs.play-with-docker.com';
